e4eb7a3ec93cbb2103baaba1d05675624a968103
ThomasOliverKimble <tokimble@gmail.com>
2022-08-09 16:06:04 +0200

    testing

--- benchmark_record_action/utils/git.py
+++ benchmark_record_action/utils/git.py
@@ -61,7 +61,7 @@ def push_directory_to_branch(source_directory, destination_directory='.', destin
     print('DESTINATION DIRECTORY: ', destination_directory)
     print('DESTINATION BRANCH: ', destination_branch)
 
-    #subprocess.check_output(['git', 'config', '--global', '--add', 'safe.directory', '/github/workspace'])
+    subprocess.check_output(['git', 'config', '--global', '--add', 'safe.directory', '/github/workspace'])
     #subprocess.check_output(['git', 'reset', '--hard'])
     subprocess.check_output(f'git checkout {destination_branch} || git checkout -b {destination_branch}', shell=True)
 

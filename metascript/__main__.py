132ca9a4c5d38e6130836a776d7a07ba88d80446
Léo Duggan <leo.duggan@alumni.epfl.ch>
2022-11-14 11:57:17 +0100

    Fixed relative import

--- metascript/__main__.py
+++ metascript/__main__.py
@@ -14,8 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from utils.webots import load_config
-from benchmark import benchmark
+from .utils.webots import load_config
+from .benchmark import benchmark
 
 
 def main():

c5bea44b640dd18648df6f4c01e0f89128ea50ef
Léo Duggan <leo.duggan@alumni.epfl.ch>
2022-11-17 15:31:56 +0100

    Finally setup my python linter

--- metascript/benchmark.py
+++ metascript/benchmark.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
+#
+# Copyright 1996-2022 Cyberbotics Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import os
 import shutil
@@ -6,7 +20,8 @@ from pathlib import Path
 from .animation import record_animations
 from .utils import git
 
-TMP_DESTINATION_DIRECTORY =  'tmp/animation'
+TMP_DESTINATION_DIRECTORY = 'tmp/animation'
+
 
 class Competitor:
     def __init__(self, id, controller_repository):
@@ -39,8 +54,8 @@ def _get_competitor():
 
     input_competitor = os.environ['INPUT_INDIVIDUAL_EVALUATION']
     competitor = Competitor(
-        id = input_competitor.split(":")[0],
-        controller_repository = input_competitor.split(":")[1].strip()
+        id=input_competitor.split(":")[0],
+        controller_repository=input_competitor.split(":")[1].strip()
     )
     print("done parsing competitor")
     return competitor
@@ -49,8 +64,10 @@ def _get_competitor():
 def _clone_competitor_controller(competitor):
     print("\nCloning competitor repo...")
 
-    competitor.controller_name = "competitor_" + competitor.id + "_" + competitor.username
-    competitor.controller_path = os.path.join('controllers', competitor.controller_name)
+    competitor.controller_name = "competitor_" + \
+        competitor.id + "_" + competitor.username
+    competitor.controller_path = os.path.join(
+        'controllers', competitor.controller_name)
 
     repo = 'https://{}:{}@github.com/{}/{}'.format(
         "Benchmark_Evaluator",
@@ -62,11 +79,13 @@ def _clone_competitor_controller(competitor):
     git.clone(repo, competitor.controller_path)
     print("done fetching repo")
 
+
 def _run_competitor_controller(config, competitor):
     print("\nRunning competitor's controller...")
     animator_controller_source = os.path.join('metascript', 'animator')
     animator_controller_destination = os.path.join('controllers', 'animator')
-    _copy_directory(animator_controller_source, animator_controller_destination)
+    _copy_directory(animator_controller_source,
+                    animator_controller_destination)
 
     # Record animation and return performance
     performance = record_animations(
@@ -79,9 +98,11 @@ def _run_competitor_controller(config, competitor):
     print('done running controller and recording animations')
     return performance
 
+
 def _update_repo_files(performance, competitor):
     _update_performance_line(performance, competitor)
     _update_animation_files(competitor)
+
 
 def _update_performance_line(performance, competitor):
 
@@ -100,21 +121,24 @@ def _update_performance_line(performance, competitor):
                 new_line = line
             # concatenate the new string and add an end-line break
             tmp_competitors = tmp_competitors + new_line + "\n"
-    
+
     with open('competitors.txt', 'w') as f:
         f.write(tmp_competitors)
 
+
 def _update_animation_files(competitor):
-    new_destination_directory = os.path.join('storage', 'wb_animation_' + competitor.id)
+    new_destination_directory = os.path.join(
+        'storage', 'wb_animation_' + competitor.id)
 
     # remove old animation
     _remove_directory(new_destination_directory)
 
     _copy_directory(TMP_DESTINATION_DIRECTORY, new_destination_directory)
     _remove_directory(TMP_DESTINATION_DIRECTORY)
-    
+
     _cleanup_storage_files(new_destination_directory)
-    return 
+    return
+
 
 def _cleanup_storage_files(directory):
     if Path(directory).exists():
@@ -127,14 +151,17 @@ def _cleanup_storage_files(directory):
             elif path.endswith('.x3d'):
                 os.rename(path, directory + '/scene.x3d')
 
+
 def _remove_tmp_files(competitor):
     _remove_directory('tmp')
     _remove_directory('metascript')
     _remove_directory(competitor.controller_path)
 
+
 def _remove_directory(directory):
     if Path(directory).exists():
         shutil.rmtree(directory)
+
 
 def _copy_directory(source, destination):
     if Path(source).exists():

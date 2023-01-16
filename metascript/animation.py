10e4452178f624c0b9a4ad923691ff747767868e
Olivier Michel <Olivier.Michel@cyberbotics.com>
2023-01-16 14:15:54 +0100

    minimize webots

--- metascript/animation.py
+++ metascript/animation.py
@@ -125,7 +125,7 @@ def record_animations(gpu, config, controller_path, participant_name, opponent_c
 
     if not gpu:
         command_line += ['xvfb-run', '-e', '/dev/stdout', '-a']
-    command_line += ['webots', '--stdout', '--stderr', '--batch', '--mode=fast',
+    command_line += ['webots', '--stdout', '--stderr', '--batch', '--minimize', '--mode=fast',
                      '--no-rendering', f'/usr/local/webots-project/{world_config["file"]}']
 
     webots_docker = subprocess.Popen(command_line, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding='utf-8')

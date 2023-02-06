8f420243ce80620db1c1c750cd2aa638262ba3d7
Olivier Michel <Olivier.Michel@cyberbotics.com>
2023-02-06 11:55:51 +0100

    Update competition.py

--- metascript/competition.py
+++ metascript/competition.py
@@ -53,7 +53,7 @@ class Participant:
                         sys.exit(1)
                 else:
                     country = self.data['country']
-                    if len(country) != 2:
+                    if country != 'demo' and len(country) != 2:
                         print(f'{message}Bad country code in {url} (you should set a two-letter country code, see https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2 for details)')
                         if not opponent:
                             sys.exit(1)

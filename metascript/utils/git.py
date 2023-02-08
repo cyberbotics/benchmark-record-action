772665f9f52ee6f27a22687bb632c612117a3eeb
Olivier Michel <Olivier.Michel@cyberbotics.com>
2023-02-08 14:08:50 +0100

    Added git pull rebase before push

--- metascript/utils/git.py
+++ metascript/utils/git.py
@@ -34,6 +34,7 @@ def push(message='Updated competition recordings'):
     )
     print(f'::group::Push new animations and ranking to https://github.com/{os.environ["GITHUB_REPOSITORY"]}')
     try:
+        print(subprocess.check_output(['git', 'pull', '--rebase'])
         if os.path.exists('participants.json'):
             subprocess.check_output(['git', 'add', '-A', 'participants.json'])
         if os.path.exists('storage'):
